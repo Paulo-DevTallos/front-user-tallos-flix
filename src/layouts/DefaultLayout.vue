65c3a3413653c30d857e1ba887e094fdc6344770
rafael mendes <rafael.mendes@tallos.com.br>
2022-10-14 15:18:00 -0300

    criando layout padrão e testando

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>navbar</div>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'defaultLayout',
+});
+</script>
+
+<style scoped>
+</style>
