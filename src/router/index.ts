6499439b8763379acec826c1c6e642fda4c64ab8
Paulo Sergio <pauloserg861@gmail.com>
2022-10-14 15:31:15 -0300

    parametrizando rota de login

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/LoginView.vue'),
     },
     {
       path: '/defaultLayout',
@@ -17,8 +17,8 @@ const router = createRouter({
           path: 'teste',
           name: 'teste',
           component: () => import('../components/teste.vue'),
-        }
-      ]
+        },
+      ],
     },
   ],
 });
