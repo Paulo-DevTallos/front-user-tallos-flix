0bedf167b3327f95f7ab7045ff30f30583c36678
Paulo Sergio <pauloserg861@gmail.com>
2022-10-17 15:16:57 -0300

    feat: TF-27 criando formulario de cadastro

--- src/components/forms/FormRegister.vue
+++ src/components/forms/FormRegister.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    
-  </div>
+  <form>
+    <div class="forms-fields">
+      <div class="input-container-style">
+        <Icon icon="carbon:user" />
+        <input type="text" placeholder="Nome do usuário" />
+      </div>
+      <div class="input-container-style">
+        <Icon icon="carbon:email" />
+        <input type="text" placeholder="E-mail" />
+      </div>
+      <div class="input-container-style">
+        <Icon icon="carbon:locked" />
+        <input type="password" placeholder="Senha" />
+        <Icon icon="carbon:view" id="view-icon" />
+      </div>
+      <div class="btn-container">
+        <button>{{ data_btn }}</button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 
+export default defineComponent({
+  name: 'FormRegister',
+  components: {
+    Icon,
+  },
+  props: {
+    data_btn: String,
+  },
+});
 </script>
 
 <style lang="scss"></style>
