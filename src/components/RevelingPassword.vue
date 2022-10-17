6827d587596a99030e3eace1fc8e51e23c837409
Paulo Sergio <pauloserg861@gmail.com>
2022-10-17 16:10:42 -0300

    criando componente de passwordReveling

---
+++ src/components/RevelingPassword.vue
@@ -0,0 +1,22 @@
+<template>
+  <span @click="$emit('toggle')">
+    <Icon icon="carbon:view" id="view-icon" v-if="isPassword" />
+    <Icon icon="carbon:view-off" id="view-icon" v-else />
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
+
+export default defineComponent({
+  name: 'RevelingPassword',
+  emits: ['toggle'],
+  props: {
+    isPassword: { type: Boolean },
+  },
+  components: {
+    Icon,
+  },
+});
+</script>
