465c304a2621f0125319ddc7eea59dfb52b11ec0
Levy <levyronald3@gmail.com>
2022-10-17 16:34:05 -0300

    fix: alterando rota de home

--- src/components/Header/HeaderApp.vue
+++ src/components/Header/HeaderApp.vue
@@ -15,7 +15,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   methods: {
     redirect() {
-      this.$router.push({ name: 'defaultLayout' });
+      this.$router.push({ name: 'home' });
     },
   },
 });
