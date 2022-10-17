f2e43e3192c403c3e2db1052e4f9043a1c671bdb
Paulo Sergio <pauloserg861@gmail.com>
2022-10-17 14:48:38 -0300

    importando componente de formulario

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -1,43 +1,37 @@
 <template>
   <div class="screen opac-scree">
     <div class="content">
-      <form>
+      <div class="form-container">
         <div class="container-logo">
           <img src="/img/logo-tallos.png" alt="Logo Tallos" />
         </div>
-        <div class="container-links">
-          <div>Entrar</div>
-          <div>Cadastrar</div>
+        <div class="container-links slide-controls">
+          <input type="radio" name="slide" id="enter" checked />
+          <input type="radio" name="slide" id="register" />
+          <label for="enter" class="slide enter">Entrar</label>
+          <label for="register" class="slide register">Cadastrar</label>
+          <div class="slider-tab"></div>
         </div>
-        <div class="forms-fields">
-          <div class="input-container-style">
-            <Icon icon="carbon:email" />
-            <input type="text" placeholder="Email" />
-          </div>
-          <div class="input-container-style">
-            <Icon icon="carbon:locked" />
-            <input type="text" placeholder="Senha" />
-            <Icon icon="carbon:view-off" id="view-icon" />
-          </div>
-          <a href="#">Esqueci a senha. Enviar por e-mail</a>
-          <div class="btn-container">
-            <button>Entrar</button>
-          </div>
-        </div>
-      </form>
+        <FormLogin :data_btn="btn_login" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Icon } from '@iconify/vue';
+import FormLogin from '../components/FormLogin.vue';
 
 export default defineComponent({
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Login',
+  name: 'LoginView',
   components: {
-    Icon,
+    FormLogin,
+  },
+
+  data() {
+    return {
+      btn_login: 'Entrar',
+    };
   },
 });
 </script>
