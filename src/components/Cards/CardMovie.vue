ca78c39d59f3d90dff651a1c23e8008735e038c5
Paulo Sergio <pauloserg861@gmail.com>
2022-10-19 10:59:45 -0300

    feat:TF-54 / TF-75 - criando pagina de movies e componente de card

---
+++ src/components/Cards/CardMovie.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Card de Filmes
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardMovie',
+});
+</script>
