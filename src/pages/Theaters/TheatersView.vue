1753274a718510fd1b1bd3f204bf9dbc115a06d3
Levy <levyronald3@gmail.com>
2022-10-19 15:31:03 -0300

    feat: componente de paginação

--- src/pages/Theaters/TheatersView.vue
+++ src/pages/Theaters/TheatersView.vue
@@ -1,7 +1,13 @@
 <template>
-  <b-container class="pt-5" fluid>
+  <b-container class="pt-5 pb-5" fluid>
     <b-row cols="1" cols-sm="2" cols-md="2" cols-lg="2">
-      <b-col v-for="movie in movies" :key="movie" col no-gutters class="mt-5">
+      <b-col
+        v-for="movie in itemsForList"
+        :key="movie.id"
+        col
+        no-gutters
+        class="mt-5"
+      >
         <b-card no-body class="overflow-hidden">
           <b-row class="d-flex justify-content-center">
             <b-col lg="4">
@@ -36,6 +42,15 @@
         </b-card>
       </b-col>
     </b-row>
+    <div class="pt-5">
+      <b-pagination
+        size="lg"
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        align="center"
+      ></b-pagination>
+    </div>
   </b-container>
 </template>
 <script lang="ts">
@@ -44,8 +59,11 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   data() {
     return {
+      perPage: 3,
+      currentPage: 1,
       movies: [
         {
+          id: 1,
           poster:
             'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR3hrCpnlQnzqbNGvUd03l72l7JM6C5GOoHvMnhsUBldIgBs7GD',
           title: 'Não! Não Olhe!',
@@ -53,6 +71,7 @@ export default defineComponent({
           imdb: 4.5,
         },
         {
+          id: 2,
           poster:
             'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSBKMx2u2RPY-FyJ_Al7C1oA7TqteTz9SK3RxR9w8LKAafIXIBx',
           title: 'TICKET to PARADISE',
@@ -60,6 +79,7 @@ export default defineComponent({
           imdb: 3.5,
         },
         {
+          id: 3,
           poster:
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLnv5c0wq18JR5YRrFI6CC_B9kB5Zqz461ZqK-LyzR6-9RiE_W',
           title: 'Minions the rise of gru',
@@ -67,6 +87,7 @@ export default defineComponent({
           imdb: 5,
         },
         {
+          id: 4,
           poster:
             'https://br.web.img3.acsta.net/c_310_420/pictures/21/11/08/16/02/3963914.png',
           title: 'Homem Aranha: sem volta pra casa',
@@ -74,6 +95,7 @@ export default defineComponent({
           imdb: 4.7,
         },
         {
+          id: 5,
           poster:
             'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSp-lzoeSCvEzSiLDBiaeBprFOMjlry4SdUot2GfS8inDd-BoyX',
           title: 'Parasita',
@@ -81,6 +103,7 @@ export default defineComponent({
           imdb: 3.7,
         },
         {
+          id: 6,
           poster:
             'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSGXk4wQrBRtCQheTNJ6L9LgQJLsJiJ76eMNlZpeTtGTJuJK3Q3',
           title: 'Jurassic World: Domínio',
@@ -88,6 +111,7 @@ export default defineComponent({
           imdb: 4,
         },
         {
+          id: 7,
           poster:
             'https://upload.wikimedia.org/wikipedia/pt/thumb/0/08/Thor_Love_and_Thunder_poster.jpg/250px-Thor_Love_and_Thunder_poster.jpg',
           title: 'Thor Amor e Trovão',
@@ -95,6 +119,7 @@ export default defineComponent({
           imdb: 3,
         },
         {
+          id: 8,
           poster:
             'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRY5QGNRfsGSClplqEptKo2HybJwUN8wspofcJvaY-eq8vteUka',
           title: 'The Batman',
@@ -103,6 +128,17 @@ export default defineComponent({
         },
       ],
     };
+  },
+  computed: {
+    rows() {
+      return this.movies.length;
+    },
+    itemsForList() {
+      return this.movies.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage,
+      );
+    },
   },
 });
 </script>
