5cd63a9e8ca4bff13d39748a8e3b9df912b54811
Paulo Sergio <pauloserg861@gmail.com>
2022-10-20 11:47:16 -0300

    feat:TF-52 criando filter bar

--- src/pages/Movies/MoviesView.vue
+++ src/pages/Movies/MoviesView.vue
@@ -3,20 +3,29 @@
     class="home-field d-flex justify-content-center align-items-center flex-column gap-5"
   >
     <div class="search-components d-flex flex-column gap-5 ">
-      <SearchBar />
+      <div class="d-flex justify-content-center">
+        <div class="me-5">
+          <button class="search-input wd-btn text-input styles-filter">
+            Filtrar...
+            <Icon icon="dashicons:arrow-down-alt2" />
+          </button>
+        </div>
+        <SearchBar />
+      </div>
       <Carousel />
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 import SearchBar from "../../components/SearchBar.vue";
 import Carousel from '@/components/Carousel.vue'
-import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'MoviesView',
-  components: { SearchBar, Carousel },
+  components: { SearchBar, Carousel, Icon },
 });
 </script>
 
