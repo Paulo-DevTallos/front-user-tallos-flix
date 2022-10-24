1baab382b7ca8e4d73c2cc0dfdcfd4f7c47cfa9e
Paulo Sergio <pauloserg861@gmail.com>
2022-10-24 11:51:35 -0300

    feat:TF-91 - criando modal de trailler e aplicando event emits

--- src/components/Modals/TraillerModal.vue
+++ src/components/Modals/TraillerModal.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    teste
+  <div class="window-modal" @click="$emit('closeWindow')">
+    <div class="iframe-content">
+      <iframe src="https://www.youtube.com/embed/faz9v2O-zGA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,7 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'TrallerModal',
+  emits: ['closeWindow'],
 });
 </script>
 
