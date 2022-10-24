3cf9d057faa9b617ca3ef7253225685cf81b39d3
Paulo Sergio <pauloserg861@gmail.com>
2022-10-24 14:34:42 -0300

    feat:TF-87 - criando componente de erro

---
+++ src/components/ErrorComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-error">
+    <h2 v-pre>Não encontramos filmes com a palavra {{ palavra }}</h2><!--lembrar de remover o v-prev-->
+    <img src="img/cuate.png" alt="imagem de erro" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ErrorComponent',
+});
+</script>
+
+<style lang="scss" scoped></style>
