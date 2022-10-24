37d40d14db7ec8a73acb8e528fe531f6c9770623
Paulo Sergio <pauloserg861@gmail.com>
2022-10-24 11:24:13 -0300

    feat:TF-91 - criando estrutura de modals

---
+++ src/components/Modals/TraillerModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    teste
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TrallerModal',
+});
+</script>
+
+<style lang="scss" scoped></style>
