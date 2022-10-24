2bca052dfc039ac894e22562f3bdccc3ebf680b9
Paulo Sergio <pauloserg861@gmail.com>
2022-10-24 11:25:26 -0300

    feat:TF-91 - implementando event emit ao componente

--- src/pages/Movies/MovieView.vue
+++ src/pages/Movies/MovieView.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="page-container">
-    <CardMovie />
-    comentarios
+    <CardMovie @traillerModal="openTraillerModal" />
+    <TraillerModal />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CardMovie from '../../components/Cards/CardMovie.vue';
+import CardMovie from '@/components/Cards/CardMovie.vue';
+import TraillerModal from '@/components/Modals/TraillerModal.vue';
 
 export default defineComponent({
-    name: "MovieView",
-    components: { CardMovie }
+  name: 'MovieView',
+  components: { CardMovie, TraillerModal },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    openTraillerModal() {
+      alert('teste');
+      console.log('teste');
+    },
+  },
 });
 </script>
 
