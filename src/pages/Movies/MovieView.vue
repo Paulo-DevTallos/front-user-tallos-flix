6cd60c1a82c4b5d4b7dc6ace84ad9bf0b06678c4
Paulo Sergio <pauloserg861@gmail.com>
2022-10-26 15:48:23 -0300

    feat:TF-133-134 - removendo trailerModal da view

--- src/pages/Movies/MovieView.vue
+++ src/pages/Movies/MovieView.vue
@@ -1,35 +1,16 @@
 <template>
   <div class="page-container">
-    <CardMovie @traillerModal="openTraillerModal" />
-    <TraillerModal
-      v-if="hiddenTraillerModal"
-      @closeWindow="closeModal"
-    />
+    <CardMovie />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import CardMovie from '@/components/Cards/CardMovie.vue';
-import TraillerModal from '@/components/Modals/TraillerModal.vue';
 
 export default defineComponent({
   name: 'MovieView',
-  components: { CardMovie, TraillerModal },
-  data() {
-    return {
-      hiddenTraillerModal: false,
-    }
-  },
-  methods: {
-    openTraillerModal(): void {
-      this.hiddenTraillerModal = true;
-    },
-
-    closeModal(): void {
-      this.hiddenTraillerModal = false;
-    },
-  },
+  components: { CardMovie },
 });
 </script>
 
