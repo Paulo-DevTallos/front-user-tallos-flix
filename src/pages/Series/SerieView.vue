a5897fc722030a2071108387314c5f5b064741b2
Paulo Sergio <pauloserg861@gmail.com>
2022-10-26 16:22:27 -0300

    feat:TF-133-134 - criando pagina de serie

---
+++ src/pages/Series/SerieView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-container">
+    <CardMovie :hiddenBtnTrailer="false" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CardMovie from '@/components/Cards/CardMovie.vue';
+
+export default defineComponent({
+  name: 'SerieView',
+  components: { CardMovie },
+});
+</script>
