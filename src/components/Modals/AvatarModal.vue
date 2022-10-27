1a1af75ea791ef4aa34c3e5108100b6c0f2723c9
Paulo Sergio <pauloserg861@gmail.com>
2022-10-27 15:54:36 -0300

    feat:TF-51 - criando componente de avatar modal

---
+++ src/components/Modals/AvatarModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box-modal">
+    Avatar do usuario
+    <button>Cancelar</button>
+    <button>Alterar</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AvatarModal',
+});
+</script>
+
+<style lang="scss" scoped></style>
