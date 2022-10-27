b3ab5cda4f1bd9b8a99a2568e46a1a2103b83102
Levy <levyronald3@gmail.com>
2022-10-27 14:53:01 -0300

    feat: componentizando card de movies

---
+++ src/components/Cards/CardsMovies.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-row cols="1" cols-sm="2" cols-md="2" cols-lg="2">
+    <b-col
+      v-for="movie in moviesRender"
+      :key="movie._id"
+      col
+      no-gutters
+      class="mt-5"
+    >
+      <b-card no-body class="overflow-hidden">
+        <b-row class="d-flex justify-content-center">
+          <b-col lg="4">
+            <b-card-img
+              :src="movie.poster || vazio.poster"
+              v-if="movie.poster ? movie.poster : vazio.poster"
+              class="rounded-0"
+            ></b-card-img>
+          </b-col>
+          <b-col lg="7">
+            <b-card-body
+              class="d-flex flex-column-reverse h-100 pb-0 card-body"
+            >
+              <div>
+                <b-card-title v-if="movie.title ? movie.title : vazio.title">{{
+                  movie.title || vazio.title
+                }}</b-card-title>
+                <b-card-subtitle>Duração: {{ movie.time }}</b-card-subtitle>
+                <div class="d-flex">
+                  <star-rating v-model="movie.imdb.rating" />
+                </div>
+                <router-link :to="{ path: `/home/movieLocation/${movie._id}` }">
+                  <b-button
+                    size="lg"
+                    class="w-75 btn-movie"
+                    @click="currentMovie(movie)"
+                  >
+                    Ver cinemas
+                  </b-button>
+                </router-link>
+              </div>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import StarRating from '../Rating/StarRating.vue';
+
+export default defineComponent({
+  components: {
+    StarRating,
+  },
+  data() {
+    return {
+      vazio: {
+        poster: '/img/empty-img.png',
+        title: 'Filme sem titulo',
+      },
+    };
+  },
+  props: {
+    moviesRender: {
+      type: Function,
+    },
+  },
+  methods: {
+    currentMovie(movie: Object) {
+      this.$store.state.Movies.currentMovie = movie;
+    },
+  }
+});
+</script>
+<style lang="scss" scoped>
+</style>
--- src/pages/Theaters/TheatersView.vue
+++ src/pages/Theaters/TheatersView.vue
@@ -1,53 +1,6 @@
 <template>
   <b-container class="pt-5 pb-5" fluid>
-    <b-row cols="1" cols-sm="2" cols-md="2" cols-lg="2">
-      <b-col
-        v-for="movie in this.$store.state.Movies.Movies.content"
-        :key="movie._id"
-        col
-        no-gutters
-        class="mt-5"
-      >
-        <b-card no-body class="overflow-hidden">
-          <b-row class="d-flex justify-content-center">
-            <b-col lg="4">
-              <b-card-img
-                :src="movie.poster || vazio.poster"
-                v-if="movie.poster ? movie.poster : vazio.poster"
-                class="rounded-0"
-              ></b-card-img>
-            </b-col>
-            <b-col lg="7">
-              <b-card-body
-                class="d-flex flex-column-reverse h-100 pb-0 card-body"
-              >
-                <div>
-                  <b-card-title
-                    v-if="movie.title ? movie.title : vazio.title"
-                    >{{ movie.title || vazio.title }}</b-card-title
-                  >
-                  <b-card-subtitle>Duração: {{ movie.time }}</b-card-subtitle>
-                  <div class="d-flex">
-                    <star-rating v-model="movie.imdb.rating" />
-                  </div>
-                  <router-link
-                    :to="{ path: `/home/movieLocation/${movie._id}` }"
-                  >
-                    <b-button
-                      size="lg"
-                      class="w-75 btn-movie"
-                      @click="currentMovie(movie)"
-                    >
-                      Ver cinemas
-                    </b-button>
-                  </router-link>
-                </div>
-              </b-card-body>
-            </b-col>
-          </b-row>
-        </b-card>
-      </b-col>
-    </b-row>
+    <cards-movies :moviesRender="this.$store.state.Movies.Movies.content" />
     <div class="pt-5 d-flex justify-content-end">
       <pagination
         class="paginationTT"
@@ -60,31 +13,24 @@
   </b-container>
 </template>
 <script lang="ts">
+import CardsMovies from '@/components/Cards/CardsMovies.vue';
 import pagination from '@/components/Pagination/PaginationPage.vue';
 import { defineComponent } from 'vue';
-import StarRating from '../../components/Rating/StarRating.vue';
 
 export default defineComponent({
   components: {
     pagination,
-    StarRating,
+    CardsMovies,
   },
   data() {
     return {
       page: 1,
       limit: 8,
-      vazio: {
-        poster: '/img/empty-img.png',
-        title: 'Filme sem titulo',
-      },
     };
   },
   computed: {
     rows() {
       return this.$store.state.Movies.Movies.numberOfElements;
-    },
-    itemsForList() {
-      return this.reloadRequest();
     },
   },
   methods: {
@@ -101,9 +47,6 @@ export default defineComponent({
     handlePageChange() {
       this.page + 1;
       this.reloadRequest();
-    },
-    currentMovie(movie: Object) {
-      this.$store.state.Movies.currentMovie = movie;
     },
   },
   mounted() {
