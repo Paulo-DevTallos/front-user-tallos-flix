bcc31e10828e63d7986e2165ae76a2f53e7a08a6
Paulo Sergio <pauloserg861@gmail.com>
2022-10-27 16:25:00 -0300

    feat:TF-51 - criando estrutura do card de avatar

--- src/components/Modals/AvatarModal.vue
+++ src/components/Modals/AvatarModal.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="box-modal">
-    Avatar do usuario
-    <button>Cancelar</button>
-    <button>Alterar</button>
+    <Icon icon="eva:close-fill" @click="$emit('closeWindow')"/>
+    <header class="header-modal">
+      Escolha seu avatar:
+    </header>
+    <div class="avatar-container">
+      <div class="roll"></div>
+      <div class="roll"></div>
+      <div class="roll"></div>
+      <div class="roll"></div>
+    </div>
+    <footer class="container-btn-modal">
+      <button @click="$emit('closeWindow')">Cancelar</button>
+      <button>Salvar</button>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 
 export default defineComponent({
   name: 'AvatarModal',
+  components: { Icon },
+  emits: ['closeWindow'],
 });
 </script>
 
