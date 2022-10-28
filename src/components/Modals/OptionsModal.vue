4eed60821a96cb483e6faf5ac56841f3476fb574
Paulo Sergio <pauloserg861@gmail.com>
2022-10-28 15:16:13 -0300

    feat:TF-147 - implementando componente de modal

--- src/components/Modals/OptionsModal.vue
+++ src/components/Modals/OptionsModal.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    Modal de opções
+  <div class="box-modal wd-modal">
+    <Icon icon="eva:close-fill" @click="$emit('closeWindow')"/>
+    <div class="avatar-container pt-4">
+      Você precisa estár logado para realizar essa ação !
+    </div>
+    <footer class="container-btn-modal pt-4">
+      <button @click="$emit('closeWindow')">OK</button>
+      <button @click="redirectToLogin">Logar</button>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 
 export default defineComponent({
   name: 'OptionsModal',
+  components: { Icon },
+  emits: ['closeWindow'],
+  methods: {
+    redirectToLogin(): void | URL {
+      return location.replace('/login');
+    }
+  }
 });
 </script>
 
