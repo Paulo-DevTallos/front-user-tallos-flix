6d52c0a33b11c2c44581ffdcd981793bd80ce89e
Paulo Sergio <pauloserg861@gmail.com>
2022-10-28 14:19:13 -0300

    feat:TF-147 - criando componente de modal de opções

---
+++ src/components/Modals/OptionsModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Modal de opções
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OptionsModal',
+});
+</script>
+
+<style lang="scss" scoped></style>
