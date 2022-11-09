ec2abb58f79f731e9072ed4eb07830537ac2c8a9
Paulo Sergio <pauloserg861@gmail.com>
2022-11-09 19:56:51 -0300

    feat:TF-132-213 - criando componente de avatar

---
+++ src/components/Avatars/Avatar.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-avatar :src="source_data" size="5rem"></b-avatar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AvatarView',
+  props: {
+    source_data: {
+      type: Boolean,
+    },
+  },
+});
+</script>
