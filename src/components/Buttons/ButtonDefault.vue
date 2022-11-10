f5cc521ebd1f89f31cc7ad41c4d382a61994787a
Paulo Sergio <pauloserg861@gmail.com>
2022-11-10 11:36:48 -0300

    feat:TF-132-213 - atribuindo props ao botao

--- src/components/Buttons/ButtonDefault.vue
+++ src/components/Buttons/ButtonDefault.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    
-  </div>
+  <b-button size="lg" class="btn-comment" @click="$emit('btnAction')">
+    {{ data_btn_title }}
+  </b-button>
 </template>
 
 <script lang="ts">
@@ -9,5 +9,11 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'ButtonDefault',
+  emits: ['btnAction'],
+  props: {
+    data_btn_title: {
+      type: String,
+    },
+  },
 });
 </script>
