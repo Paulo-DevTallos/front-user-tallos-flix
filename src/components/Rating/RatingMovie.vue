06e0a4595238ee04bedf951dd4ed2d596d52ee0f
Levy <levyronald3@gmail.com>
2022-11-11 16:44:09 -0300

    feat: implementando componente de avaliação

--- src/components/Rating/RatingMovie.vue
+++ src/components/Rating/RatingMovie.vue
@@ -1,1 +1,33 @@
-<template>avaliações</template>
+<template>
+  <div class="theaters-form-container">
+    <p class="plot-title ps-5 mt-4">Avalie este Filme</p>
+    <hr class="orange-line-separator" />
+    <div class="d-flex flex-column align-items-center">
+      <div>Avaliação</div>
+      <div class="d-flex">
+        <star-rating v-model="avaliation" :showControl="true" />
+      </div>
+      <div>
+        <button-default
+          class="Btn-salvar"
+          :data_btn_title="'Salvar'"
+          @btnAction="SaveRating"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ButtonDefault from '../Buttons/ButtonDefault.vue';
+import StarRating from './StarRating.vue';
+
+export default defineComponent({
+  components: { StarRating, ButtonDefault },
+  data() {
+    return {
+      avaliation: 0,
+    };
+  },
+});
+</script>
