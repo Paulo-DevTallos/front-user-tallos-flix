6cb8f03536a44734ed7a4ef0cc54273cd9dba828
Paulo Sergio <pauloserg861@gmail.com>
2022-11-16 15:51:24 -0300

    feat:TF-269-274 - implementando logica de renderizacao de titulo

--- src/components/Carousel/SlideCarousel.vue
+++ src/components/Carousel/SlideCarousel.vue
@@ -1,160 +1,117 @@
 <template>
-	<div class="container-full">
-		<h4>Nos Cinemas</h4>
-		<button @click="prev" class="arrow-left">
-			<Icon icon="carbon:chevron-left" />	
-		</button>
-		<button @click="next" class="arrow-right">
-			<Icon icon="carbon:chevron-right" />
-		</button>
-		<div class="carousel-container">
-			<div
-				v-for="movie in $store.state.Movies.Movies.content"
-				:key="movie._id"
-				class="gallery-wrapper"
-			>
-				<div class="gallery-movies">
-					<img
-						class="item-image-poster"
-						v-if="movie.poster ? movie.poster : empty.poster"
-						:src="movie.poster || empty.poster"
-						alt="movie.title"
-					/>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="container-full">
+    <h4>{{ title_carousel }}</h4>
+    <button @click="prev" class="arrow-left">
+      <Icon icon="carbon:chevron-left" />
+    </button>
+    <button @click="next" class="arrow-right">
+      <Icon icon="carbon:chevron-right" />
+    </button>
+    <div class="carousel-container">
+      <div
+        v-for="movie in $store.state.Movies.Movies.content"
+        :key="movie._id"
+        class="gallery-wrapper"
+      >
+        <div class="gallery-movies">
+          <img
+            class="item-image-poster"
+            v-if="movie.poster ? movie.poster : empty.poster"
+            :src="movie.poster || empty.poster"
+            alt="movie.title"
+          />
+        </div>
+        <div class="card-info-movies">
+          <h4
+            v-if="
+              movie.title.length > 16
+                ? (substring = `${movie.title.substring(0, 16)}...`)
+                : (substring = `${movie.title.substring(0, 16)} `)
+            "
+          >
+          	{{ substring }}
+          </h4>
+          <p>
+            Duração:
+            {{
+              Math.trunc(movie.runtime / 60) +
+              'h' +
+              (movie.runtime % 60) +
+              'min'
+            }}
+          </p>
+					<StarRating class="rating" :ratingRawValue="movie.imdb.rating" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Icon } from '@iconify/vue';
+import StarRating from '../Rating/StarRating.vue';
 
 export default defineComponent({
-	name: 'SlideCarousel',
-	components: { Icon },
+  name: 'SlideCarousel',
+  components: { Icon, StarRating },
+  props: {
+    title_carousel: {
+      type: String,
+    },
+  },
   data() {
-		return {
-			movies: [],
+    return {
+      movies: [],
       empty: {
-				poster: '/img/empty-img.png',
+        poster: '/img/empty-img.png',
       },
+      substring: '',
     };
   },
 
   methods: {
-		next() {
-			const items = document.querySelectorAll('.item-image-poster');
-			let currentItem = 0;
-			const maxItems = items.length;
+    next() {
+      const items = document.querySelectorAll('.item-image-poster');
+      let currentItem = 0;
+      const maxItems = items.length;
 
-			currentItem--
+      currentItem--;
 
-			if (currentItem >= maxItems) {
-				currentItem = 0;
-			}
+      if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
 
-			if (currentItem < 0) {
-				currentItem = maxItems - 1;
-			}
+      if (currentItem < 0) {
+        currentItem = maxItems - 1;
+      }
 
-			items[currentItem].scrollIntoView({
-				behavior: "smooth",
-				block: 'nearest',
-			});
-		},
+      items[currentItem].scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    },
 
-		prev() {
-			const items = document.querySelectorAll('.item-image-poster');
-			let currentItem = 0;
-			const maxItems = items.length;
+    prev() {
+      const items = document.querySelectorAll('.item-image-poster');
+      let currentItem = 0;
+      const maxItems = items.length;
 
-			currentItem++;
+      currentItem++;
 
-			if (currentItem <= maxItems) {
-				currentItem = 0;
-			}
+      if (currentItem <= maxItems) {
+        currentItem = 0;
+      }
 
-			if (currentItem > 0) {
-				currentItem = maxItems + 1;
-			}
+      if (currentItem > 0) {
+        currentItem = maxItems + 1;
+      }
 
-			items[currentItem].scrollIntoView({
-				behavior: "smooth",
-				block: 'nearest',
-			});
-		},
-	}
+      items[currentItem].scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    },
+  },
 });
 </script>
-
-<style lang="scss" scoped>
-.container-full {
-	position: relative;
-	max-width: 1280px;
-
-	h4 {
-		margin-left: 9%;
-		margin-bottom: 0;
-		font-family: 'Nunito', Arial, Helvetica, sans-serif;
-		font-weight: 500;
-		font-size: 30px;
-		color: #fff;
-	}
-
-	.arrow-left,
-	.arrow-right {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		position: absolute;
-		top: 50%;
-		transform: translateY(-50%);
-		left: 0;
-		right: auto;
-		bottom: 0;
-		font-size: 20px;
-		width: 40px;
-		height: 40px;
-		text-align: center;
-		color: #009acc;
-		cursor: pointer;
-		border: none;
-		border-radius: 100%;
-		opacity: 1;
-		background: #212121;
-		transition: all 600ms ease-in-out;
-	}
-
-	.arrow-right {
-		left: auto;
-		right: 0;
-		background-image: linear-gradient(to right, transparent 0%, black 200%);
-	}
-
-	.carousel-container {
-		padding: 15px;
-		position: relative;
-		max-width: 84%;
-		margin: 0 auto;
-		overflow-x: hidden;
-		display: flex;
-		.gallery-wrapper {
-			display: flex;
-			justify-content: center;
-			.gallery-movies {
-				display: flex;
-				flex-flow: row wrap;
-				gap: 15px;
-				padding: 0 40px 40px 0;
-
-				.item-image-poster {
-					width: 230px;
-					height: 330px;
-					flex-shrink: 0;
-				}
-			}
-		}
-	}
-}
-</style>
