054dddb4b54c3b9525bf5d11adf61d8b983c30c2
Paulo Sergio <pauloserg861@gmail.com>
2022-11-18 16:09:49 -0300

    ajustando pagina de erro e componente de erro

--- src/components/ErrorComponent.vue
+++ src/components/ErrorComponent.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="container-error">
-    <h2 class="error-text-message">
-      Não encontramos filmes com a palavra {{ data_word }}
-    </h2>
-    <!--lembrar de remover o v-prev-->
-    <img src="/img/cuate.png" alt="imagem de erro" />
-  </div>
+	<div class="container-error">
+		<h2 class="error-text-message">{{ error_value }} {{ data_word }}</h2>
+		<!--lembrar de remover o v-prev-->
+		<img src="/img/cuate.png" alt="imagem de erro" />
+	</div>
+	<!--Não encontramos filmes com a palavra-->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'ErrorComponent',
-  props: {
-    data_word: String,
-  },
+	name: 'ErrorComponent',
+	props: {
+		data_word: String,
+		error_value: String,
+	},
 });
 </script>
 
 <style lang="scss" scoped>
 .error-text-message {
-  color: #cbd6db;
-  font-family: 'Nunito', sans-serif;
+	color: #cbd6db;
+	font-family: 'Nunito', sans-serif;
 }
 </style>
