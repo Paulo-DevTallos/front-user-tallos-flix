d54451d7950b41c5de6b411512787881a3f9d649
Levy <levyronald3@gmail.com>
2022-11-18 15:28:42 -0300

    feat: logica para exibir filmes avaliados pelo usuario

--- src/pages/avaliable/avaliableUser.vue
+++ src/pages/avaliable/avaliableUser.vue
@@ -1,77 +1,71 @@
 <template>
-  <div class="container-fv">
-    <header-app />
-    <b-container class="pt-5 favorite-container">
-      <div class="pb-2">
-        <h3 class="title">Minhas Avaliaçõedadass</h3>
-      </div>
-      <b-row col no-gutters>
-        <b-col
-          class="Colprin"
-          v-for="avaliable in moviesAvaliable"
-          :key="avaliable"
-        >
-          <b-row>
-            <b-col>
-              <b-card-img :src="avaliable.data.result.poster"></b-card-img>
-            </b-col>
-          </b-row>
-        </b-col>
-      </b-row>
-    </b-container>
-    <!-- <b-container class="pt-5">
-      <div class="pb-2">
-        <h3 class="title">Minhas Avaliações</h3>
-      </div>
-      <b-row
-        v-for="avaliable in moviesAvaliable"
-        :key="avaliable"
-        col
-        no-gutters
-        class=""
-      >
-        <b-col cols="2">teste</b-col>
-      </b-row>
-    </b-container> -->
-  </div>
+	<div class="container-fv">
+		<header-app />
+		<b-container class="pt-5 favorite-container">
+			<div class="pb-2">
+				<h3 class="title">Minhas Avaliações</h3>
+			</div>
+			<b-row col no-gutters>
+				<b-col
+					cols="6"
+					class="Colprin pb-5"
+					v-for="avaliable in moviesAvaliable"
+					:key="avaliable"
+				>
+					<b-row>
+						<b-col cols="6">
+							<b-card-img :src="avaliable.data.result.poster"></b-card-img>
+						</b-col>
+						<b-col>
+							<h5>{{ avaliable.data.result.title }}</h5>
+							<h6>Sua avaliação</h6>
+							<star-rating class="d-flex" />
+							<h6 class="btnRemove">Remover a avaliação</h6>
+						</b-col>
+					</b-row>
+				</b-col>
+			</b-row>
+		</b-container>
+	</div>
 </template>
 <script lang="ts">
 import HeaderApp from '@/components/Header/HeaderApp.vue';
 import { defineComponent } from 'vue';
 import ServiceGetRatingMovie from '@/services/axios/MovieRequests';
+import StarRating from '@/components/Rating/StarRating.vue';
 
 export default defineComponent({
-  components: { HeaderApp },
-  data() {
-    return {
-      moviesAvaliable: [],
-    };
-  },
-  mounted() {
-    console.log(this.$store.state.Ratings.RatingsUser);
-    this.$store.dispatch(
-      'Ratings/getAllRatingsUser',
-      this.$store.state.Users.UserId,
-    );
-    for (
-      let index = 0;
-      index < this.$store.state.Ratings.RatingsUser.length;
-      index++
-    ) {
-      ServiceGetRatingMovie.getMovieFilterId(
-        this.$store.state.Ratings.RatingsUser[index].movie_id,
-      )
-        .then((result) => {
-          this.moviesAvaliable.push(result);
-        })
-        .catch((err) => {});
-      // this.$store.dispatch(
-      //   'Movies/getMovieFilterId',
-      //   this.$store.state.Ratings.RatingsUser[index].movie_id,
-      // );
-      // console.log(this.$store.state.Movies.Movies);
-    }
-    console.log(this.moviesAvaliable);
-  },
+	components: { HeaderApp, StarRating },
+	data() {
+		return {
+			moviesAvaliable: [],
+		};
+	},
+	mounted() {
+		console.log(this.$store.state.Ratings.RatingsUser);
+		this.$store.dispatch(
+			'Ratings/getAllRatingsUser',
+			this.$store.state.Users.UserId,
+		);
+		for (
+			let index = 0;
+			index < this.$store.state.Ratings.RatingsUser.length;
+			index++
+		) {
+			ServiceGetRatingMovie.getMovieFilterId(
+				this.$store.state.Ratings.RatingsUser[index].movie_id,
+			)
+				.then((result) => {
+					this.moviesAvaliable.push(result);
+				})
+				.catch((err) => {});
+			// this.$store.dispatch(
+			//   'Movies/getMovieFilterId',
+			//   this.$store.state.Ratings.RatingsUser[index].movie_id,
+			// );
+			// console.log(this.$store.state.Movies.Movies);
+		}
+		console.log(this.moviesAvaliable);
+	},
 });
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,96 +1,96 @@
 import {
-  createRouter,
-  createWebHistory,
-  type NavigationGuardNext,
-  type RouteLocation,
+	createRouter,
+	createWebHistory,
+	type NavigationGuardNext,
+	type RouteLocation,
 } from 'vue-router';
 
 const authGuard =
-  () => (to: RouteLocation, from: RouteLocation, next: NavigationGuardNext) => {
-    if (localStorage.getItem('token') || '') {
-      next();
-    } else {
-      next('/');
-    }
-  };
+	() => (to: RouteLocation, from: RouteLocation, next: NavigationGuardNext) => {
+		if (localStorage.getItem('token') || '') {
+			next();
+		} else {
+			next('/');
+		}
+	};
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../pages/LoginView.vue'),
-    },
-    {
-      path: '/home/favorites',
-      name: 'favorites',
-      beforeEnter: authGuard(),
-      component: () => import('../pages/Favorites/MyFavorites.vue'),
-    },
-    {
-      path: '/home/avaliable',
-      name: 'avaliable',
-      beforeEnter: authGuard(),
-      component: () => import('../pages/avaliable/avaliable.vue'),
-    },
-    {
-      path: '/home',
-      name: 'default',
-      component: () => import('../layouts/DefaultLayout.vue'),
-      children: [
-        {
-          path: '',
-          name: 'home',
-          component: () => import('../pages/HomeView.vue'),
-        },
-        {
-          path: 'theaters',
-          name: 'theaters',
-          component: () => import('../pages/Theaters/TheatersView.vue'),
-        },
-        {
-          path: 'movie',
-          name: 'movie',
-          component: () => import('../pages/Movies/MovieView.vue'),
-        },
-        {
-          path: 'movies',
-          name: 'movies',
-          component: () => import('../pages/Movies/MoviesView.vue'),
-        },
-        {
-          path: 'serie',
-          name: 'serie',
-          component: () => import('../pages/Series/SerieView.vue'),
-        },
-        {
-          path: 'series',
-          name: 'series',
-          component: () => import('../pages/Series/SeriesView.vue'),
-        },
-        {
-          path: 'movieLocation/:id',
-          name: 'movieLocation',
-          component: () => import('../pages/Theaters/MovieLocation.vue'),
-        },
-        {
-          path: 'Peoples/:name',
-          name: 'DirectorPage',
-          component: () => import('../pages/Peoples/PeopleView.vue'),
-        },
-      ],
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('@/pages/NotFound/NotFoundView.vue'),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			redirect: '/home',
+		},
+		{
+			path: '/login',
+			name: 'login',
+			component: () => import('../pages/LoginView.vue'),
+		},
+		{
+			path: '/home/favorites',
+			name: 'favorites',
+			beforeEnter: authGuard(),
+			component: () => import('../pages/Favorites/MyFavorites.vue'),
+		},
+		{
+			path: '/home/avaliable',
+			name: 'avaliable',
+			beforeEnter: authGuard(),
+			component: () => import('../pages/avaliable/avaliableUser.vue'),
+		},
+		{
+			path: '/home',
+			name: 'default',
+			component: () => import('../layouts/DefaultLayout.vue'),
+			children: [
+				{
+					path: '',
+					name: 'home',
+					component: () => import('../pages/HomeView.vue'),
+				},
+				{
+					path: 'theaters',
+					name: 'theaters',
+					component: () => import('../pages/Theaters/TheatersView.vue'),
+				},
+				{
+					path: 'movie',
+					name: 'movie',
+					component: () => import('../pages/Movies/MovieView.vue'),
+				},
+				{
+					path: 'movies',
+					name: 'movies',
+					component: () => import('../pages/Movies/MoviesView.vue'),
+				},
+				{
+					path: 'serie',
+					name: 'serie',
+					component: () => import('../pages/Series/SerieView.vue'),
+				},
+				{
+					path: 'series',
+					name: 'series',
+					component: () => import('../pages/Series/SeriesView.vue'),
+				},
+				{
+					path: 'movieLocation/:id',
+					name: 'movieLocation',
+					component: () => import('../pages/Theaters/MovieLocation.vue'),
+				},
+				{
+					path: 'Peoples/:name',
+					name: 'DirectorPage',
+					component: () => import('../pages/Peoples/PeopleView.vue'),
+				},
+			],
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'not-found',
+			component: () => import('@/pages/NotFound/NotFoundView.vue'),
+		},
+	],
 });
 
 export default router;
